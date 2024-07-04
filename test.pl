e12b284b6f8427869399a11c58f1ae2f4ca21fa4
flybd63 <flybd63@gmail.com>
2024-07-04 17:41:00 +0900

    Create test.pl

---
+++ test.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use LWP::UserAgent;
+use JSON;
+
+my $api_key = 'YOUR_ALPHA_VANTAGE_API_KEY';
+my $symbol = 'AAPL';
+my $output_file = 'cross_results.json';
+
+# 株価データの取得
+sub get_stock_data {
+    my ($symbol, $api_key) = @_;
+    my $ua = LWP::UserAgent->new;
+    my $url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=$symbol&apikey=$api_key";
+    my $response = $ua->get($url);
+
+    die "Failed to get data: ", $response->status_line unless $response->is_success;
+
+    my $data = decode_json($response->decoded_content);
+    my $time_series = $data->{'Time Series (Daily)'};
+    
+    my @prices;
+    foreach my $date (sort keys %$time_series) {
+        push @prices, $time_series->{$date}->{'4. close'};
+    }
+    return @prices;
+}
+
+# 移動平均の計算
+sub moving_average {
+    my ($prices, $days) = @_;
+    my @ma;
+    for my $i (0..$#$prices) {
+        if ($i >= $days - 1) {
+            my $sum = 0;
+            $sum += $prices->[$_] for ($i - $days + 1..$i);
+            push @ma, $sum / $days;
+        } else {
+            push @ma, undef;
+        }
+    }
+    return @ma;
+}
+
+# ゴールデンクロス・デッドクロスの検出
+sub detect_cross {
+    my ($prices, $ma) = @_;
+    my %result = (golden_cross => 0, dead_cross => 0);
+    for my $i (1..$#$prices) {
+        if (defined $ma->[$i] && defined $ma->[$i-1]) {
+            if ($prices->[$i] > $ma->[$i] && $prices->[$i-1] <= $ma->[$i-1]) {
+                $result{golden_cross} = 1;
+            } elsif ($prices->[$i] < $ma->[$i] && $prices->[$i-1] >= $ma->[$i-1]) {
+                $result{dead_cross} = 1;
+            }
+        }
+    }
+    return %result;
+}
+
+# メインルーチン
+sub main {
+    #my @prices = get_stock_data($symbol, $api_key);
+    #my @ma75 = moving_average(\@prices, 75);
+    #my %result = detect_cross(\@prices, \@ma75);
+	my %result = ( "aaa" => "bbb" );
+
+    open my $fh, '>', $output_file or die "Could not open file '$output_file': $!";
+    print $fh encode_json(\%result);
+    close $fh;
+}
+
+main();
