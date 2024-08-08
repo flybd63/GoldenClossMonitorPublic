6977bb49c5330bbdc0920884b08ecb9899b97440
flybd63 <flybd63@gmail.com>
2024-08-08 17:54:27 +0900

    Add files via upload

--- get_golden_cloth_from_yahoo.pl
+++ get_golden_cloth_from_yahoo.pl
@@ -4,23 +4,40 @@ use warnings;
 
 use JSON;
 use Time::Piece;
-
+use DateTime;
 
 my $threshold = 90;
 my $today = localtime->strftime('%Y%m%d');
 
+
 # メインルーチン
 my %tikers = %{&load_mst()};
 my $len = scalar(keys %tikers);
-my $count = 1;
+my $count = 0;
 my %result = ();
 foreach my $t (sort keys %tikers){
-    #print STDERR "$count/$len t:$t\n";
+    $count++;
+    
+    if($tikers{$t}{class} !~ /^プライム/ &&
+       $tikers{$t}{class} !~ /^スタンダード/ &&
+       $tikers{$t}{class} !~ /^グロース/ ){
+        next;
+    }
+
+    print STDERR "$count/$len t:$t $tikers{$t}{name} $tikers{$t}{class}\n";
     my $symbol = "$t.T";
 
     my @prices = get_stock_data($symbol);
+
+    if($#prices < 74){
+        print STDERR "  - prices is short. skip. len: $#prices\n";
+        next;
+    }
+    
+    my @ma25 = moving_average(\@prices, 25);
     my @ma75 = moving_average(\@prices, 75);
-    my %r = detect_cross(\@prices, \@ma75, $threshold);
+
+    my %r = detect_cross(\@prices, \@ma25, \@ma75, $threshold);
     
     if($r{golden_cross} == 1 || $r{dead_cross} == 1 || $r{golden_cross_near} == 1 || $r{dead_cross_near} == 1){
         $result{$t} = ();
@@ -30,11 +47,18 @@ foreach my $t (sort keys %tikers){
         $result{$t}{dead_cross_near} = $r{dead_cross_near};
     }
 
-    $count ++;
     #last;
 }
 
-print encode_json(\%result);
+my $now = DateTime->now(time_zone => 'GMT');
+my $formatted_date = $now->strftime('%Y-%m-%dT%H:%M:%S');
+
+my %out = (
+    date_modified => $formatted_date,
+    result => \%result
+    );
+
+print encode_json(\%out);
 
 #&save_json($today, encode_json(\%result));
 #&save_json("latest", encode_json(\%result));
@@ -81,24 +105,35 @@ sub save_json {
 # 株価データの取得
 sub get_stock_data {
     my ($symbol) = @_;
-    
-    my $url = '"https://query2.finance.yahoo.com/v8/finance/chart/7011.T?range=6mo&interval=1d"';
-    #my $url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=$symbol&apikey=$api_key";
-    #my $response = $ua->get($url);
-    my $response = `curl -sS $url`;
 
-    #die "Failed to get data: ", $response->status_line unless $response->is_success;
+    my @prices = ();
 
-    #my $data = decode_json($response->decoded_content);
-    #print STDERR "ret:$response";
-    my $data = decode_json($response);
-    my @prices = reverse( @{$data->{chart}{result}[0]{indicators}{quote}[0]{close}} );
-    splice(@prices, 75);
-    
-    
-#    foreach my $date (sort keys %$time_series) {
-#        push @prices, $time_series->{$date}->{'4. close'};
-#    }
+    eval{
+        my $url = '"https://query2.finance.yahoo.com/v8/finance/chart/'.$symbol.'?range=6mo&interval=1d"';
+        
+        #my $url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=$symbol&apikey=$api_key";
+        #my $response = $ua->get($url);
+        my $response = `curl -sS $url`;
+
+        #die "Failed to get data: ", $response->status_line unless $response->is_success;
+
+        #my $data = decode_json($response->decoded_content);
+        #print STDERR "ret:$response";
+        my $data = decode_json($response);
+        #my @prices = reverse( @{$data->{chart}{result}[0]{indicators}{quote}[0]{close}} );
+        #splice(@prices, 76);
+
+        @prices = @{$data->{chart}{result}[0]{indicators}{quote}[0]{close}};
+        
+
+        
+        #    foreach my $date (sort keys %$time_series) {
+        #        push @prices, $time_series->{$date}->{'4. close'};
+        #    }
+    };
+    if($@){
+        print STDERR "ERROR:$@";
+    }
     return @prices;
 }
 
@@ -109,38 +144,50 @@ sub moving_average {
     for my $i (0..$#$prices) {
         if ($i >= $days - 1) {
             my $sum = 0;
-            $sum += $prices->[$_] for ($i - $days + 1..$i);
+            for (my $j = $i - $days + 1; $j <= $i; $j++) {
+                my $p = 0;
+                if(defined $prices->[$j]){
+                    $p = $prices->[$j];
+                }
+                $sum += $p;
+            }
+                #$sum += $prices->[$_] for ($i - $days + 1..$i);
             push @ma, $sum / $days;
         } else {
             push @ma, undef;
         }
     }
+
+    #print STDERR "ma$days: @ma\n";
     return @ma;
 }
 
 # ゴールデンクロス・デッドクロスの検出
 sub detect_cross {
-    my ($prices, $ma, $threshold) = @_;
+    my ($prices, $ma25, $ma75, $threshold) = @_;
     my %result = (golden_cross => 0, dead_cross => 0, golden_cross_near => 0, dead_cross_near => 0);
     my $total_price = @$prices;
-    for my $i (1..$#$prices) {
-        if (defined $ma->[$i] && defined $ma->[$i-1]) {
-            my $price_ma_diff = abs($prices->[$i] - $ma->[$i]);
-            if ($prices->[$i] > $ma->[$i] && $prices->[$i-1] <= $ma->[$i-1]) {
-                $result{golden_cross} = 1;
-            } elsif ($prices->[$i] < $ma->[$i] && $prices->[$i-1] >= $ma->[$i-1]) {
-                $result{dead_cross} = 1;
-            }
-            if ($price_ma_diff <= $threshold) {
-                my $proximity = 100 * (1 - $price_ma_diff / $threshold);
-                if ($prices->[$i] > $ma->[$i]) {
-                    $result{golden_cross_near} = $proximity;
-                } elsif ($prices->[$i] < $ma->[$i]) {
-                    $result{dead_cross_near} = $proximity;
-                }
-            }
+    my $i = $#$prices;
+    #for my $i (1..$#$prices) {
+    #next unless defined $ma25->[$i] && defined $ma75->[$i];
+
+    my $price_ma_diff = abs($prices->[$i] - $ma75->[$i]);
+
+    if ($ma25->[$i - 1] < $ma75->[$i - 1] && $ma25->[$i] > $ma75->[$i]) {
+        $result{golden_cross} = 1;
+    } elsif ($ma25->[$i - 1] > $ma75->[$i - 1] && $ma25->[$i] < $ma75->[$i]) {
+        $result{dead_cross} = 1;
+    }
+    if ($price_ma_diff <= $threshold) {
+        my $proximity = 100 * (1 - $price_ma_diff / $threshold);
+        if ($prices->[$i] > $ma75->[$i]) {
+            $result{golden_cross_near} = $proximity;
+        } elsif ($prices->[$i] < $ma75->[$i]) {
+            $result{dead_cross_near} = $proximity;
         }
     }
+
+    #}
     return %result;
 }
 
